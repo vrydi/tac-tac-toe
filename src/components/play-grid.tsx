e564ba01a00a50d0d61f0929140f6fa5e5742762
vrydi <57484019+vrydi@users.noreply.github.com>
2023-03-27 21:32:40 +0200

    added winchecks

--- src/components/play-grid.tsx
+++ src/components/play-grid.tsx
@@ -9,6 +9,8 @@ export function PlayGrid(props: { turnX: boolean; switchTurn: () => void }) {
 
   function onFieldClick(rowNumber: number, columnNumber: number): void {
     board[rowNumber][columnNumber] = props.turnX ? "x" : "o";
+    const win = checkForWin(board);
+    console.log(win);
     setBoard([...board]);
     props.switchTurn();
   }
@@ -49,4 +51,52 @@ function PlayGridField(props: {
       {!props.content.includes(".") && props.content}
     </div>
   );
+}
+
+function checkForWin(board: string[][]) {
+  if (checkRows(board)) return true;
+  const transposedBoard = transpose(board);
+  if (checkRows(transposedBoard)) return true;
+  if (checkDiagonal(board)) return true;
+  return false;
+}
+
+function checkRows(board: string[][]): boolean {
+  let flag = false;
+  board.forEach((row: string[]) => {
+    switch (JSON.stringify(row)) {
+      case JSON.stringify(["x", "x", "x"]):
+      case JSON.stringify(["o", "o", "o"]):
+        flag = true;
+        break;
+    }
+  });
+  return flag;
+}
+
+function checkDiagonal(board: string[][]): boolean {
+  if (board[0][0] === "x" && board[1][1] === "x" && board[2][2] === "x")
+    return true;
+  if (board[0][2] === "x" && board[1][1] === "x" && board[2][0] === "x")
+    return true;
+  if (board[0][0] === "o" && board[1][1] === "o" && board[2][2] === "o")
+    return true;
+  if (board[0][2] === "o" && board[1][1] === "o" && board[2][0] === "o")
+    return true;
+  return false;
+}
+
+function transpose(matrix: any[][]): any[][] {
+  const rows = matrix.length,
+    cols = matrix[0].length;
+  const grid = [];
+  for (let j = 0; j < cols; j++) {
+    grid[j] = Array(rows);
+  }
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      grid[j][i] = matrix[i][j];
+    }
+  }
+  return grid;
 }
